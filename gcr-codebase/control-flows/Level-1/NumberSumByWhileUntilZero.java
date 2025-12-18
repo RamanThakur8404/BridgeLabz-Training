b794ee18d79c6a63c40783b09b4f88a76f91064c
Raman <ramanthakur8404@gmail.com>
2025-12-18 21:11:49 +0530

    [Raman] : added program to find sum of numbers until user enter 0

--- gcr-codebase/control-flows/Level-1/NumberSumByWhileUntilZero.java
+++ gcr-codebase/control-flows/Level-1/NumberSumByWhileUntilZero.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 public class NumberSumByWhileUntilZero {
 	public static void main(String []args) {
-		Scanner input = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		// taking user input and assign variable total as 0.0
 		double total = 0.0;
 		System.out.println("Enter the number: ");
-		double value = input.nextDouble();
+		double value = scanner.nextDouble();
 		
 		//apply looping statement
 		while(value!=0){
 		total += value;
 		System.out.println("Enter the number: ");
-		value = input.nextDouble();
+		value = scanner.nextDouble();
 		}
 		
 		//printing the output and close the scanner object
 		System.out.println("The total value is :" + total);
+		scanner.close();
 	}
 }	
 		
