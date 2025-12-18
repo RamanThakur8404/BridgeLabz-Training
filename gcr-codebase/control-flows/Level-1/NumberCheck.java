4bc5dc79a1193f8d50a422da14958ee097592e2f
Raman <ramanthakur8404@gmail.com>
2025-12-18 21:06:20 +0530

    [Raman] : identify the number is positive, negative or zero

--- gcr-codebase/control-flows/Level-1/NumberCheck.java
+++ gcr-codebase/control-flows/Level-1/NumberCheck.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class NumberCheck {
 	public static void main(String []args) {
-		Scanner read = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the number : ");
-		int number = read.nextInt();
+		int number = input.nextInt();
 		
 		//check type of number using conditional statement
 		if(number > 0 ){
@@ -17,6 +17,6 @@ public class NumberCheck {
 		}
 
 		//close the scanner object
-		read.close();
+		input.close();
 	}
 }	
