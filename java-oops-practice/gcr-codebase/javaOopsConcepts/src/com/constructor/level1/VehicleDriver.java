9b872a88911e4dd0bb7330a5c7afc701d53da924
Raman <ramanthakur8404@gmail.com>
2025-12-28 22:33:26 +0530

    Improve driver validation in VehicleDriver

--- java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/constructor/level1/VehicleDriver.java
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/constructor/level1/VehicleDriver.java
@@ -2,21 +2,20 @@ package com.constructor.level1;
 
 //VehicleDriver.java
 public class VehicleDriver {
- public static void main(String[] args) {
+	public static void main(String[] args) {
 
-     // Creating vehicle objects
-     Vehicle vehicle1 = new Vehicle("Robin", "Car");
-     Vehicle vehicle2 = new Vehicle("Celvin", "Bike");
+		Vehicle vehicle1 = new Vehicle("Robin", "Car");
+		Vehicle vehicle2 = new Vehicle("Celvin", "Bike");
 
-     // Display initial vehicle details
-     vehicle1.displayVehicleDetails();
-     vehicle2.displayVehicleDetails();
+		// Display initial vehicle details
+		vehicle1.displayVehicleDetails();
+		vehicle2.displayVehicleDetails();
 
-     // Update registration fee for all vehicles
-     Vehicle.updateRegistrationFee(6000);
+		// Update registration fee for all vehicles
+		Vehicle.updateRegistrationFee(6000);
 
-     // Display details after updating registration fee
-     vehicle1.displayVehicleDetails();
-     vehicle2.displayVehicleDetails();
+		// Display details after updating registration fee
+		vehicle1.displayVehicleDetails();
+		vehicle2.displayVehicleDetails();
  }
 }
