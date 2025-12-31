cd9a1bb66aedf47de8dc35be78fe0931de3722ce
Raman <ramanthakur8404@gmail.com>
2026-01-01 00:10:34 +0530

    [Raman] : add Order class to aggregate multiple products for a customer

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/Order.java
@@ -0,0 +1,27 @@
+package com.objectmodeling;
+
+import java.util.ArrayList;
+
+class Order {
+    private int orderId;
+    private ArrayList<Product> products = new ArrayList<>();
+
+    public Order(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void showOrderDetails() {
+        System.out.println("Order ID: " + orderId);
+        double total = 0;
+
+        for (Product p : products) {
+            System.out.println("- " + p.getProductName() + " : " + p.getPrice());
+            total += p.getPrice();
+        }
+        System.out.println("Total Amount: " + total);
+    }
+}
