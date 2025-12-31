8b2c9edc47e5f4515f511c1f61d706b5d373fdb7
Raman <ramanthakur8404@gmail.com>
2026-01-01 00:01:02 +0530

    [Raman] : add Patient class to represent patients consulting doctors

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/Patient.java
@@ -0,0 +1,29 @@
+package com.objectmodeling;
+
+import java.util.ArrayList;
+
+public class Patient {
+    private int patientId;
+    private String name;
+    private ArrayList<Doctor> doctors = new ArrayList<>();
+
+    public Patient(int patientId, String name) {
+        this.patientId = patientId;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addDoctor(Doctor doctor) {
+        doctors.add(doctor);
+    }
+
+    public void viewDoctors() {
+        System.out.println("Patient: " + name + " consulted:");
+        for (Doctor d : doctors) {
+            System.out.println("- Dr. " + d.getName());
+        }
+    }
+}
