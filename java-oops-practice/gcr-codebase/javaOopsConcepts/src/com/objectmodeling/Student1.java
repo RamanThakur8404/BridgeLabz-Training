1f5dc48c16702d2a737d11a1a207916920ec3580
Raman <ramanthakur8404@gmail.com>
2026-01-01 00:20:39 +0530

    [Raman] : add Student class to enroll in courses

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/Student1.java
@@ -0,0 +1,32 @@
+package com.objectmodeling;
+
+import java.util.ArrayList;
+
+public class Student1 {
+    private int rollNo;
+    private String name;
+    private ArrayList<Course1> courses = new ArrayList<>();
+
+    public Student1(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Communication method
+    public void enrollCourse(Course1 course) {
+        courses.add(course);
+        course.addStudent(this);
+        System.out.println(name + " enrolled in " + course.getCourseName());
+    }
+
+    public void viewCourses() {
+        System.out.println("Student: " + name + " enrolled courses:");
+        for (Course1 c : courses) {
+            System.out.println("- " + c.getCourseName());
+        }
+    }
+}
