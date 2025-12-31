d002f9be8d9dc9255f29e5650cdffe59fd6e2308
Raman <ramanthakur8404@gmail.com>
2026-01-01 00:11:08 +0530

    [Raman] : add ECommerceDriver to demonstrate customer placing orders with products

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/Ecommerce.java
@@ -0,0 +1,32 @@
+package com.objectmodeling;
+
+public class Ecommerce {
+    public static void main(String[] args) {
+
+        Customer1 customer = new Customer1(1, "Rohan");
+        Customer1 customer1 = new Customer1(2, "Mohan");
+
+        Product p1 = new Product(101, "Laptop", 55000);
+        Product p2 = new Product(102, "Mouse", 800);
+        Product p3 = new Product(103, "Keyboard", 1500);
+
+        Order order1 = new Order(1001);
+        order1.addProduct(p1);
+        order1.addProduct(p2);
+
+        Order order2 = new Order(1002);
+        order2.addProduct(p3);
+
+        customer.placeOrder(order1);
+        customer.placeOrder(order2);
+
+        System.out.println();
+        customer.viewOrders();
+        
+        customer1.placeOrder(order1);
+        customer1.placeOrder(order2);
+
+        System.out.println();
+        customer1.viewOrders();
+    }
+}
