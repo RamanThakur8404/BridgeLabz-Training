680aa25232f240eb7885d5d0ebf54696011673fb
Raman <ramanthakur8404@gmail.com>
2026-01-01 00:21:06 +0530

    [Raman] : add Course class to store course details and link students/professors

---
+++ java-oops-practice/gcr-codebase/javaOopsConcepts/src/com/objectmodeling/Course1.java
@@ -0,0 +1,33 @@
+package com.objectmodeling;
+import java.util.ArrayList;
+public class Course1 {
+    private String courseName;
+    private Professor professor;   // Aggregation
+    private ArrayList<Student1> students = new ArrayList<>();
+
+    public Course1(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public void addStudent(Student1 student) {
+        students.add(student);
+    }
+
+    public void showCourseDetails() {
+        System.out.println("\nCourse: " + courseName);
+        System.out.println("Professor: " +
+                (professor != null ? professor.getName() : "Not Assigned"));
+        System.out.println("Enrolled Students:");
+        for (Student1 s : students) {
+            System.out.println("- " + s.getName());
+        }
+    }
+}
