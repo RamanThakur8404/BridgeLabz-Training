cbb62d4b3a63736d3bb9795e09cf71d485388e4a
Raman <ramanthakur8404@gmail.com>
2026-01-10 11:52:33 +0530

    [Raman] : added TreeSetSearch file

---
+++ java-dsa-practice/gcr-codebase/javaDsaConcepts/src/com/datastructure/runtimeanalysis/datastructures/TreeSetSearch.java
@@ -0,0 +1,10 @@
+package com.datastructure.runtimeanalysis.datastructures;
+
+import java.util.TreeSet;
+
+public class TreeSetSearch {
+	public static boolean search(TreeSet<Integer> set, int target) {
+		return set.contains(target);
+	}
+}
+	
