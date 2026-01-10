8d02b3fcb9b873470a27a9f5001fb74ed8f47ac8
Raman <ramanthakur8404@gmail.com>
2026-01-10 12:06:36 +0530

    [Raman] : added StringConcat file

---
+++ java-dsa-practice/gcr-codebase/javaDsaConcepts/src/com/datastructure/runtimeanalysis/stringconcat/StringConcat.java
@@ -0,0 +1,11 @@
+package com.datastructure.runtimeanalysis.stringconcat;
+public class StringConcat {
+
+    public static void concatenate(int n) {
+        String s = "";
+
+        for (int i = 0; i < n; i++) {
+            s = s + "hello";
+        }
+    }
+}
