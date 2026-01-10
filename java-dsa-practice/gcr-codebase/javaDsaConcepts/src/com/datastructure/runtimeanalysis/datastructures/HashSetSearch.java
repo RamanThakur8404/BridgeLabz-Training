dc29b89e49894ebc51808965f08e3ad118f90d09
Raman <ramanthakur8404@gmail.com>
2026-01-10 11:52:17 +0530

    [Raman] : added HashSetSearch file

---
+++ java-dsa-practice/gcr-codebase/javaDsaConcepts/src/com/datastructure/runtimeanalysis/datastructures/HashSetSearch.java
@@ -0,0 +1,10 @@
+package com.datastructure.runtimeanalysis.datastructures;
+
+import java.util.HashSet;
+
+public class HashSetSearch {
+
+	public static boolean search(HashSet<Integer> set, int target) {
+		return set.contains(target);
+	}
+}
