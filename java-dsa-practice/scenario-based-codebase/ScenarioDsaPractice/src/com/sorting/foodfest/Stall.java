f0904a36f7058aa312d6293b2b767aaa9673a5b5
Raman <ramanthakur8404@gmail.com>
2026-01-24 22:30:01 +0530

    [Raman] : added food fest scenario problem

---
+++ java-dsa-practice/scenario-based-codebase/ScenarioDsaPractice/src/com/sorting/foodfest/Stall.java
@@ -0,0 +1,10 @@
+package com.sorting.foodfest;
+class Stall {
+    String name;
+    int foot_fall;
+
+    Stall(String name, int foot_fall) {
+        this.name = name;
+        this.foot_fall = foot_fall;
+    }
+}
