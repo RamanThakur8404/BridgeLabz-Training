877e56938dfccdbbedfb5985e23c8bc291dc3940
Raman <ramanthakur8404@gmail.com>
2026-01-27 23:20:57 +0530

    [Raman] : added Bank Account testing using Junit

---
+++ java-collections-practice/gcr-codebase/Junit/src/test/java/com/Junit/BankAccountTest.java
@@ -0,0 +1,68 @@
+package com.Junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+public class BankAccountTest {
+
+    private BankAccount account;
+
+    @BeforeEach
+    void setUp() {
+        account = new BankAccount(1000);
+    }
+
+    @Test
+    void testDeposit() {
+        account.deposit(500);
+        assertEquals(1500, account.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        account.withdraw(300);
+        assertEquals(700, account.getBalance());
+    }
+
+    @Test
+    void testWithdrawInsufficientFunds() {
+
+        Executable executable = new Executable() {
+            @Override
+            public void execute() {
+                account.withdraw(2000);
+            }
+        };
+
+        assertThrows(RuntimeException.class, executable);
+    }
+
+    @Test
+    void testInvalidDeposit() {
+
+        Executable executable = new Executable() {
+            @Override
+            public void execute() {
+                account.deposit(-100);
+            }
+        };
+
+        assertThrows(IllegalArgumentException.class, executable);
+    }
+
+    @Test
+    void testInvalidWithdraw() {
+
+        Executable executable = new Executable() {
+            @Override
+            public void execute() {
+                account.withdraw(-50);
+            }
+        };
+
+        assertThrows(IllegalArgumentException.class, executable);
+    }
+}
