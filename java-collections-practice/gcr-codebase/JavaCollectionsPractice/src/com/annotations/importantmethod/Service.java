22bd9cf896f03518e1324662551ba01928e108c6
Raman <ramanthakur8404@gmail.com>
2026-01-28 14:28:44 +0530

    [Raman] :Demonstrating the use of @ImportantMethod annotation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/annotations/importantmethod/Service.java
@@ -0,0 +1,11 @@
+package com.annotations.importantmethod;
+public class Service {
+
+    @ImportantMethod
+    public void criticalTask() {}
+
+    @ImportantMethod(level = "LOW")
+    public void minorTask() {}
+
+    public void normalTask() {}
+}
