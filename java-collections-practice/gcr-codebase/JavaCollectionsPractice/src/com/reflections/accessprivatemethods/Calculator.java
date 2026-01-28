09ed1ae7483dedc41c0d1f5d572e75f2a7e092d2
Raman <ramanthakur8404@gmail.com>
2026-01-28 09:55:51 +0530

    [Raman] : add Reflection example to invoke private method in Calculator class

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflections/accessprivatemethods/Calculator.java
@@ -0,0 +1,7 @@
+package com.reflections.accessprivatemethods;
+// Class with private method
+class Calculator {
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflections/accessprivatemethods/ReflectionPrivateMethodExample.java
@@ -0,0 +1,23 @@
+package com.reflections.accessprivatemethods;
+import java.lang.reflect.Method;
+
+public class ReflectionPrivateMethodExample {
+    public static void main(String[] args) throws Exception {
+
+        // Load class
+        Class<?> cls = Calculator.class;
+
+        // Create object
+        Calculator c = new Calculator();
+
+        // Access private method
+        Method m = cls.getDeclaredMethod("multiply", int.class, int.class);
+        m.setAccessible(true);   // allow private access
+
+        // Invoke method
+        Object result = m.invoke(c, 4, 5);
+
+        // Display result
+        System.out.println("Result: " + result);
+    }
+}
