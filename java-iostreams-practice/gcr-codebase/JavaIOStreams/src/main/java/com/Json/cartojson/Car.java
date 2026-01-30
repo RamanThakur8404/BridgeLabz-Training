c3d59c3f56704b55159b766a639c6cd984f903b7
Raman <ramanthakur8404@gmail.com>
2026-01-31 00:03:03 +0530

    [Raman] : add Convert the car object to json file

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreams/src/main/java/com/Json/cartojson/Car.java
@@ -0,0 +1,12 @@
+package com.Json.cartojson;
+class Car {
+    public String brand;
+    public String model;
+    public int year;
+
+    Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+}
